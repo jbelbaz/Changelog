c5653e09ae2df3612802fb9b069843e76d3ab794
jbelbaz <32191774+jbelbaz@users.noreply.github.com>
2020-04-22 09:19:25 +0200

    add changelog.Sh

---
+++ changelog.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+FROM_TAG="$1"
+TO_TAG="$2"
+SPACE="cRAcZ4D1Cr4PP6acwqjQWU" # meveo
+WORKDIR="/mnt/y/Opencell/Sources/Community Clone"
+CURRENT_DIR="$(realpath $(pwd))"
+API_KEY="02dbd0635a36cbbeea48"
+API_SECRET="9e525204512e4ad65dd89b946542173f473db656"
+TODAY=$(date +'%d/%m/%Y')
+
+FILE_BASE="tickets_${FROM_TAG}_${TO_TAG}"
+TICKETS_LIST=${CURRENT_DIR}/${FILE_BASE}.lst
+MODEL_TICKETS=${CURRENT_DIR}/model_${FILE_BASE}.lst
+API_TICKETS=${CURRENT_DIR}/api_${FILE_BASE}.lst
+SERVICE_TICKETS=${CURRENT_DIR}/service_${FILE_BASE}.lst
+GUI_TICKETS=${CURRENT_DIR}/gui_${FILE_BASE}.lst
+BUG_TICKETS=${CURRENT_DIR}/bug_${FILE_BASE}.lst
+ALL_TICKETS=${CURRENT_DIR}/all_${FILE_BASE}.lst
+ALL_TICKETS_MD=${CURRENT_DIR}/CHANGELOG.md
+
+if [ -z "$FROM_TAG" -o -z "$TO_TAG" ]
+then
+  echo "usage: assembla_tickets.sh <from_tag> <to_tag>" >&2
+  exit 1
+fi
+
+# Prep files
+echo -e "\n# Changelog\nAll notable changes to this project will be documented in this file.\n">> "$ALL_TICKETS"
+echo -e "## [$2] - $(date +'%d/%m/%y')\n">>"$ALL_TICKETS"
+echo -e "\n### Model Tickets">"$MODEL_TICKETS"
+echo -e "\n### Api Tickets">"$API_TICKETS"
+echo -e "\n### Service Tickets">"$SERVICE_TICKETS"
+echo -e "\n### GUI Tickets">"$GUI_TICKETS"
+echo -e "\n### Bug Tickets">"$BUG_TICKETS"
+
+cd "${WORKDIR}"
+echo ">>> Fetching origin"
+git fetch --prune --tags origin
+
+# Get ticket list between tags
+echo ">>> From $FROM_TAG"
+git log -n 1 $FROM_TAG
+
+echo ">>> To $TO_TAG"
+git log -n 1 $TO_TAG
+
+git log ${FROM_TAG}..${TO_TAG} --decorate --pretty="format:%s" | grep -E "#[0-9]+" | sed -E 's/[^#]*#([0-9]+).*/\1/' | sort -u >"$TICKETS_LIST"
+
+# Build ticket data file
+ticketsCount=$(wc -l <"$TICKETS_LIST")
+echo "Fetching data for $ticketsCount tickets"
+rm "$TICKETS" 2>/dev/null
+currentTicket=0
+
+
+
+cat "$TICKETS_LIST" | while read ticketNumber
+do
+    currentTicket=$(( $currentTicket + 1 ))
+    echo Ticket "core#${ticketNumber} (${currentTicket}/${ticketsCount})"
+    json=$( curl -H "X-Api-Key: ${API_KEY}" -H "X-Api-Secret: ${API_SECRET}" https://api.assembla.com/v1/spaces/cRAcZ4D1Cr4PP6acwqjQWU/tickets/${ticketNumber} )
+    number=$(echo $json | jq -c '.number')
+    type=$(echo $json | jq -c '.custom_fields.Type' | sed 's/"//g')
+    summary=$(echo $json | jq -c '.summary' | sed 's/"//g')
+    component=$(echo $json | jq -c '.custom_fields.Component' | sed 's/"//g')
+    link="https://opencell.assembla.com/spaces/meveo/tickets/"$number
+    tofile="[#$number]($link) - $summary"
+
+    echo "$tofile" >> "$ALL_TICKETS"
+
+    if [ "$type" == "Enhancement" ]; then
+        if [ "$component" == "Opencell-API" ]; then
+            echo "$tofile" >> "$API_TICKETS"
+        fi
+        if [ "$component" == "Opencell-Model" ]; then
+            echo "$tofile" >> "$MODEL_TICKETS"
+        fi
+        if [ "$component" == "Opencell-Services" ]; then
+            echo "$tofile" >> "$SERVICE_TICKETS"
+        fi
+        if [ "$component" == "Opencell-Admin" ]; then
+            echo "$tofile" >> "$GUI_TICKETS"
+        fi
+    else
+         echo "$tofile" >> "$BUG_TICKETS"
+    fi
+done
+
+echo -e "\n"
+echo -e "------------------------\n"
+echo -e "Model tickets: \n"
+echo -e "------------------------\n"
+
+cat $MODEL_TICKETS>>"$ALL_TICKETS"
+
+echo -e "\n"
+echo -e "------------------------\n"
+echo -e "API tickets: \n"
+echo -e "------------------------\n"
+
+cat $API_TICKETS>>"$ALL_TICKETS"
+
+echo -e "\n"
+echo -e "------------------------\n"
+echo -e "Service tickets: \n"
+echo -e "------------------------\n"
+
+cat $SERVICE_TICKETS>>"$ALL_TICKETS"
+
+echo -e "\n"
+echo -e "------------------------\n"
+echo -e "GUI tickets: \n"
+echo -e "------------------------\n"
+
+cat $GUI_TICKETS>>"$ALL_TICKETS"
+
+
+echo -e "\n"
+echo -e "------------------------\n"
+echo -e "BUG tickets: \n"
+echo -e "------------------------\n"
+
+cat $BUG_TICKETS>>"$ALL_TICKETS"
+
+cat $ALL_TICKETS>>"$ALL_TICKETS_MD"
