928def76bab695c51cf5783d2c829d97871fa45a
jbelbaz <32191774+jbelbaz@users.noreply.github.com>
2020-04-30 13:29:11 +0200

    0430

--- liquibase.sh
+++ liquibase.sh
@@ -15,13 +15,11 @@ pwd
 FROM_TAG=$2
 TO_TAG=$3
 WORKSPACE=.
-FILE_NAME="LIQUIBASE.md"
+CURRENT_DIR="$(realpath $(pwd))"
+FILE_NAME=${CURRENT_DIR}/LIQUIBASE.csv
 SQL_FILE_NAME="${FROM_TAG//./_}_TO_${TO_TAG//./_}.sql"
 STRUCTURE_CHANGE_LOG="$1/opencell-model/src/main/db_resources/changelog/current/structure.xml"
 DATA_CHANGE_LOG="$1/opencell-model/src/main/db_resources/changelog/current/data.xml"
-TITLE="# Liquibase"
-DESCRIPTION="All notable database updates to this project will be documented in this file."
-TODAY=$(date +'%d/%m/%Y')
 
 ## Remove file
 rm -rf $SQL_FILE_NAME
@@ -33,9 +31,7 @@ git checkout $FROM_TAG -f
 ids=()
 IFS=$'§'
 changeSets=( $(xmllint --format --xpath "//*[local-name() = 'changeSet']" $STRUCTURE_CHANGE_LOG | sed 's/changeSet>/changeSet>§/g'))
-echo "-- changesets 11 defined"
 changeSets+=( $(xmllint --format --xpath "//*[local-name() = 'changeSet']" $DATA_CHANGE_LOG | sed 's/changeSet>/changeSet>§/g'))
-echo "-- changesets 12 defined"
 for changeSet in "${changeSets[@]}"
     do
         ids+=($(echo $changeSet | xmllint --format --xpath "string(//*/@id)" -))
@@ -43,28 +39,20 @@ for changeSet in "${changeSets[@]}"
 printf '%s\n' "${ids[@]}"
 
 
-IFS=$OLD_IFS    
-echo -e "$TITLE\n$DESCRIPTION\n" > ../$FILE_NAME
-echo -e "## [$TO_TAG] - $TODAY\n" >> ../$FILE_NAME
-echo $INCLUDE >> ../$FILE_NAME
+IFS=$OLD_IFS
 for k in $(git tag -l  --sort=v:refname); do 
     if [  $k \> "$FROM_TAG" -a \( $k \< "$TO_TAG" -o $k == "$TO_TAG" \) ]; then
         git checkout $k -f
         echo "-- checkout $k "
         IFS=$'§'
         changeSets=( $(xmllint --format --xpath "//*[local-name() = 'changeSet']" $STRUCTURE_CHANGE_LOG | sed 's/changeSet>/changeSet>§/g'))
-        echo "-- changesets 21 defined"
         changeSets+=( $(xmllint --format --xpath "//*[local-name() = 'changeSet']" $DATA_CHANGE_LOG | sed 's/changeSet>/changeSet>§/g'))
-        echo "-- changesets 22 defined"
+        echo "-- changeset(s) set"
         for changeSet in "${changeSets[@]}"
         do
-            echo "-- $changeSet"
             id=$(echo $changeSet | xmllint --format --xpath "string(//*/@id)" -)
-            echo "-- id defined"
             ticket=$(echo $id | sed 's/#//g' | sed -E 's/([0-9]*)_.*/\1/')
-            echo "-- ticket defined"
-            changeSetNoXML=$(echo $changeSet | perl -pe 's/<changeSet.*?>//g' | perl -pe 's/<(?=[a-zA-Z]*?\s)/;/g' | perl -pe's/<([a-zA-Z]*?)>/;/g' | perl -pe 's/<\/[a-zA-Z]*?>|<\/[a-zA-Z]*?>|<|\/>|>//g')
-            echo "-- changeSetNoXML defined"
+            changeSetNoXML=$(echo $changeSet | perl -pe 's/<changeSet.*?>//g' | perl -pe 's/<(?=[a-zA-Z]*?\s)/;/g' | perl -pe 's/<([a-zA-Z]*?)>/;/g' | perl -pe 's/<\/[a-zA-Z]*?>|<\/[a-zA-Z]*?>|<|\/>|>//g' | perl -pe 's/\t|\n/ /g')
             if [[ !" ${ids[@]} " =~ ${id} ]]; then
                 echo -n "-- DUPLICATE CHANGESET : "
                 echo $id
@@ -72,13 +60,10 @@ for k in $(git tag -l  --sort=v:refname); do
                 ids+=($id)
                 echo -n "-- ADDING CHANGESET : "
                 echo $id
-                echo -e "[#$ticket](https://opencell.assembla.com/spaces/meveo/tickets/$ticket)$changeSetNoXML\n" >> ../$FILE_NAME
+                echo -e "#$ticket - https://opencell.assembla.com/spaces/meveo/tickets/$ticket$changeSetNoXML" >> "$FILE_NAME"
             fi
         done
         #printf '%s\n' "${ids[@]}"
     fi
 done
-liquibase --url=jdbc:postgresql://localhost:5432/meveo2?outputLiquibaseSql=true --username=meveo --password=meveo --classpath=postgresql-42.1.4.jar dropAll
-liquibase --url=jdbc:postgresql://localhost:5432/meveo2?outputLiquibaseSql=true --username=meveo --password=meveo --classpath=postgresql-42.1.4.jar --changeLogFile="../$FILE_NAME" --outputFile="../$SQL_FILE_NAME" clearCheckSums updatesql
-
 IFS=$OLD_IFS
